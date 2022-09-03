01682faf14dd12a38cbd51c1a1967074c30a9a0f
A.L <xenide@users.noreply.github.com>
2022-09-03 21:46:15 +0200

    test moar tests

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.7.0;
 
 import "forge-std/Test.sol";
 
 import "src/LogCompression.sol";
+import "src/StableOracleMath.sol";
 
 contract CounterTest is Test {
 
@@ -12,14 +13,15 @@ contract CounterTest is Test {
     }
     
     function testLowResLog() public view {
-        uint amount = 30001203981203981209381;
+        // uint amount = 3000120398120398120;
+        uint amount = 1e6;
         int256 res = LogCompression.toLowResLog(amount);
         console.logInt(res);
         console2.logUint(uint256(res));
     }
 
     function testFromLowResLog() public view {
-        int logAmount = 30291;
+        int logAmount = -30291;
 
         uint256 res = LogCompression.fromLowResLog(logAmount);
 
@@ -27,4 +29,19 @@ contract CounterTest is Test {
         console2.log(res/1e18);
     }
 
+    function testRollTime() public 
+    {
+        for (uint i = 0; i < 20; ++i) {
+            console.log(block.number);
+            console.log(block.timestamp);
+            vm.roll(block.number + 1);
+            vm.warp(block.timestamp + 5);
+        }
+    }
+
+    function testCalcSpotPrice() public {
+        (int256 lLogSpotPrice, int256 lLogBptPrice) = StableOracleMath._calcLogPrices(1000, 1e18, 1e18, 2e18);
+
+        console.logInt(lLogSpotPrice);
+    }
 }
